e1399166552e92438e4f3eda85996728c04b668c
灵乌未默 <tigertan1998@gmail.com>
2026-02-05 01:06:31 +0800

    去掉严格模式

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,9 @@ import './index.css'
 import { I18nProvider } from './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <I18nProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </I18nProvider>
-  </React.StrictMode>,
 )
